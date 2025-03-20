e8faddd101bbadf069532c7b28e45db8a5a3e3a8
Filipe Durães <fhdduraes@gmail.com>
2025-03-20 11:59:29 -0300

    Add basic project structure

---
+++ Workshop.Client/src/main/java/com/filipeduraes/workshop/client/WorkshopClient.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.filipeduraes.workshop.client;
+
+import com.filipeduraes.workshop.core.Workshop;
+
+/**
+ *
+ * @author fhdd
+ */
+public class WorkshopClient 
+{
+    public static void main(String[] args) 
+    {
+        Workshop workshop = new Workshop();
+        System.out.println("Hello World!");
+    }
+}
