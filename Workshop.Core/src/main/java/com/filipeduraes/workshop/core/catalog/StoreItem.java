fb6d0af47ff0d4d3edadbf7a94f8cd346ca35c30
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2025-05-03 18:39:43 -0300

    Update StoreItem.java

--- Workshop.Core/src/main/java/com/filipeduraes/workshop/core/catalog/StoreItem.java
+++ Workshop.Core/src/main/java/com/filipeduraes/workshop/core/catalog/StoreItem.java
@@ -5,6 +5,8 @@ package com.filipeduraes.workshop.core.catalog;
 import java.util.UUID;
 
 /**
+ * Representa um item disponível para venda na loja, estendendo a classe Product.
+ * Esta classe adiciona funcionalidade para gerenciamento de estoque ao produto base.
  *
  * @author Filipe Durães
  */
@@ -12,10 +14,19 @@ public class StoreItem extends Product
 {
     private int stockAmount;
 
+    /**
+     * Cria uma nova instância de StoreItem.
+     *
+     * @param id Identificador único do item
+     * @param name Nome do item
+     * @param description Descrição detalhada do item
+     * @param price Preço do item
+     * @param stockAmount Quantidade disponível em estoque
+     */
     public StoreItem(UUID id, String name, String description, String price, int stockAmount)
     {
         super(id, name, description, price);
-        
+
         this.stockAmount = stockAmount;
     }
 }
