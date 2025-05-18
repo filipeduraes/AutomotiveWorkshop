c28ba483959165943d0f56dd228f2153b24f44b2
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2025-05-18 16:24:47 -0300

    Delete Workshop/Workshop.Client.ViewModel/target/reports directory

--- Workshop/Workshop.Client.ViewModel/target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.filipeduraes.workshop.client.utils"},{"l":"com.filipeduraes.workshop.client.viewmodel"}];updateSearchResults();
